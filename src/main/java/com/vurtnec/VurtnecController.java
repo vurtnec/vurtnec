db4e796e5c8c68a7f5c74947346c7ddbb7aa6596
vurtnec2015 <296519473@qq.com>
2015-02-21 15:03:57 +0800

    test project version

---
+++ src/main/java/com/vurtnec/VurtnecController.java
@@ -0,0 +1,16 @@
+package com.vurtnec;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VurtnecController
+{
+
+  @RequestMapping(value={"/welcome"}, method={org.springframework.web.bind.annotation.RequestMethod.GET})
+  public String welcome() {
+	System.out.println("test");
+    return "/welcome";
+  }
+
+}
